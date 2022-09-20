d65201d157a89b2eb88b25ed7b5b5be827452514
irfaimuhammad <muhammadirfai170202@gmail.com>
2022-09-20 16:30:33 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Pertama</title>
+</head>
+<body>
+
+<p>Latihan Pertama saya belajar framework Codeighniter</p>
+<p>Framework Codeighniter sangat mudah dipelajari</p>
+
+</body>
+</html>
